292c1c86653d1a3ba44c28c9e5127188ea7744b5
Kevin <kevincespedes2004@gmail.com>
2022-07-18 21:19:08 -0500

    añadiendo mejoras al codigo

--- src/pages/SearchPokemon.jsx
+++ src/pages/SearchPokemon.jsx
@@ -2,20 +2,44 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { pokemonApi } from "../api/pokemonApi";
 import { FormPokemon } from "../components/FormPokemon";
-import { LogoPokemon } from "../components/LogoPokemon";
 import { Spinner } from "../components/Spinner";
+import { Sex } from "../components/Sex";
+import { LogoPokemon } from "../components/LogoPokemon";
 import { addZero } from "../helpers";
 
 import styled from "./SearchPokemon.module.css";
 
 export const SearchPokemon = () => {
-
   let navigate = useNavigate();
 
   const [dataPokemon, setDataPokemon] = useState({});
   const [isLoading, setIsLoading] = useState(false);
 
-  const { id: idPokemon, name, sprites } = dataPokemon;
+  const {
+    id: idPokemon,
+    name,
+    sprites,
+    weight,
+    abilities = [],
+    height,
+    types,
+  } = dataPokemon;
+
+  const writeTypes = () => {
+    let types = [];
+
+    if (dataPokemon.types) {
+      dataPokemon.types.forEach((type) => {
+        types.push(
+          <p className={styled.pokemon__attributes__atribute}>
+            {type.type.name}
+          </p>
+        );
+      });
+    }
+
+    return types;
+  };
 
   const { id } = useParams();
 
@@ -35,36 +59,36 @@ export const SearchPokemon = () => {
     getPokemon();
   }, [id]);
 
-   const handleToBack = () => {
-      navigate("/");
-   }
+  const handleToBack = () => {
+    navigate("/");
+  };
 
   return (
-    <div className={styled.container}>
-      <header>
-        <LogoPokemon />
-        <nav 
-          className={styled.nav}
-        >
-          <i 
-            class={`fa-solid fa-angle-left ${styled.icon_back}`}
-            onClick={handleToBack}
-          ></i>
-          <FormPokemon />
-        </nav>
-      </header>
+    <>
+      <div className={styled.container}>
+        <header>
+          <LogoPokemon />
+          <nav className={styled.nav}>
+            <i
+              className={`fa-solid fa-angle-left ${styled.icon_back}`}
+              onClick={handleToBack}
+            ></i>
+            <FormPokemon />
+          </nav>
+        </header>
+      </div>
       {isLoading ? (
         <Spinner />
       ) : (
-        <div className={styled.pokemon_search}>
-          <div>
+        <>
+          <div className={`${styled.pokemon_search} ${styled.container}`}>
             <div className={styled.pokemon__header}>
               <span className={styled.pokemon__id}>
                 {idPokemon && addZero(idPokemon)}
               </span>
               <span className={styled.pokemon__name}>{name}</span>
             </div>
-            <div className={styled.pokemon_body}>
+            <div className={styled.pokemon__body}>
               <figure>
                 <img
                   src={sprites?.other?.home["front_default"]}
@@ -72,14 +96,33 @@ export const SearchPokemon = () => {
                 />
               </figure>
             </div>
-            <div>
-              <p>Fuego</p>
-              <p>Bolador</p>
+            <div className={styled.pokemon__attributes}>{writeTypes()}</div>
+          </div>
+          <div className={styled.pokemon__statistics}>
+            <div className={styled.content__statistics}>
+              <div>
+                <h3>Altura</h3>
+                <p>{height / 10}m</p>
+              </div>
+              <div>
+                <h3>Categoría</h3>
+              </div>
+              <div>
+                <h3>Peso</h3>
+                <p>{weight / 10}kg</p>
+              </div>
+              <div>
+                <h3>Habilidad</h3>
+                {abilities[0]?.ability.name}
+              </div>
+              <div>
+                <h3>Sexo</h3>
+                <Sex />
+              </div>
             </div>
           </div>
-          <div className={styled.pokemon__statistics}>aqui estadisticas</div>
-        </div>
+        </>
       )}
-    </div>
+    </>
   );
 };
